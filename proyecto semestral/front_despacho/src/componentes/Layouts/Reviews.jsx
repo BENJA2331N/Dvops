6de1c7645cc32835f1c6026d323c13a831b3df27
BENJA2331N <ben.nunezs@duocuc.cl>
2026-06-17 09:54:45 -0400

    Codigo arreglado , problemas con los logos solucionado

--- proyecto semestral/front_despacho/src/componentes/Layouts/Reviews.jsx
+++ proyecto semestral/front_despacho/src/componentes/Layouts/Reviews.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import logo1 from "../../assets/images/logo1.png";
+import logo2 from "../../assets/images/logo2.png";
+import logo3 from "../../assets/images/logo3.png";
+
 function Reviews() {
   return (
     <div className="bg-white  sm:py-10">
@@ -5,29 +10,30 @@ function Reviews() {
         <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
           Empresas que confían en nosotros
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 mx-auto"
-            src="https://seeklogo.com/images/A/apple-logo-E3DBF3AE34-seeklogo.com.png"
-            alt="Transistor"
-            width="158"
-            height="48"
-          />
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div className="flex justify-center">
+            <img
+              className="col-span-2 max-h-16 w-auto object-contain lg:col-span-1 mx-auto"
+              src={logo1}
+              alt="Logo 1"
+            />
+          </div>
 
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 mx-auto"
-            src="https://seeklogo.com/images/H/hyper-x-logo-C4B6774A38-seeklogo.com.png"
-            alt="Tuple"
-            width="158"
-            height="48"
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 mx-auto"
-            src="https://seeklogo.com/images/L/logitech-gaming-logo-B76FC713B0-seeklogo.com.png"
-            alt="SavvyCal"
-            width="158"
-            height="48"
-          />
+          <div className="flex justify-center">
+            <img
+              className="col-span-2 max-h-16 w-auto object-contain lg:col-span-1 mx-auto"
+              src={logo2}
+              alt="Logo 2"
+            />
+          </div>
+
+          <div className="flex justify-center">
+            <img
+              className="col-span-2 max-h-16 w-auto object-contain lg:col-span-1 mx-auto"
+              src={logo3}
+              alt="Logo 3"
+            />
+          </div>
         </div>
       </div>
     </div>
