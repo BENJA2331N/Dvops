ef294c1308058e00c14709f658f91ef8c624deef
l.ugal3 <l.ugal3.dev@gmail.com>
2026-06-25 02:32:16 +0200

    fix: elimina linea file duplicada en pipelines

--- proyecto semestral/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/config/CorsConfig.java
+++ proyecto semestral/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/config/CorsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 import org.springframework.lang.NonNull;
 
 @Configuration
